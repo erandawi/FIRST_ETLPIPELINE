a0b4434fd015b45000fbdd334d85e507c2ddea12
erandawi <erandalakshan3567@gmail.com>
2025-02-26 22:11:17 +0530

    etl.py

---
+++ dags/etl.py
@@ -0,0 +1,6 @@
+from airflow import DAG
+from airflow.providers.ttp.operators.http import SimpleHttpOperator
+from airflow.decorators import task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.dates import days_ago
+import json
